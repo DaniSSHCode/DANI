086918a570b8a86ba1a7e19b03c5b0e4b35bae8b
Experimentos Dani <dsandoval.code@gmail.com>
2019-01-31 22:26:48 -0500

    testing

--- papi/script.js
+++ papi/script.js
@@ -4,7 +4,13 @@ $(document).ready(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // is mobile..
         var sheet = document.createElement('style')
-        sheet.innerHTML = ".text-content {margin-top: 0px; margin-left: 0%; margin-right: 0%; text-align: left; font-size: 18px;}";
+        sheet.innerHTML = `
+        .text-content {margin-top: 0px; margin-left: 0%; margin-right: 0%; text-align: left; font-size: 18px;} 
+        .card-image img {
+            width: 100px;
+            height: auto;
+        };`
+        
         document.body.appendChild(sheet);
     }
 });
